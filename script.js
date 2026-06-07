2bc0a4162bc9a39efcc6d96ad4809d0b01b457f1
Helen <kozovyakina@gmail.com>
2026-06-07 14:04:46 +0300

    change

--- script.js
+++ script.js
@@ -4951,31 +4951,31 @@ $ - конец строки (после последовательностью �
 // console.log(validPassword("$qwERqq5qq"));
 
 
-let but = document.querySelector("#but");
-but.addEventListener("click", smsUser);
+// let but = document.querySelector("#but");
+// but.addEventListener("click", smsUser);
 
-function smsUser(){
-    let name = document.querySelector("#your_name").value;
-    let text = document.querySelector("#text").value;
-    let regExpBBMail = /(.*?)<(b|u|i|s|h1|p)>(.*?)<(\/\2)>(.*?)/ig;
-    text = text.replace(regExpBBMail, "$1 <span style='color:red'>&lt;$2&gt;</span>$3<span style='color:red'>&lt;$4&gt;</span> $5");
-    // 
-    console.log(text);
+// function smsUser(){
+//     let name = document.querySelector("#your_name").value;
+//     let text = document.querySelector("#text").value;
+//     let regExpBBMail = /(.*?)<(b|u|i|s|h1|p)>(.*?)<(\/\2)>(.*?)/ig;
+//     text = text.replace(regExpBBMail, "$1 <span style='color:red'>&lt;$2&gt;</span>$3<span style='color:red'>&lt;$4&gt;</span> $5");
+//     // 
+//     console.log(text);
 
-    // document.writeln(`
-    //     <fieldset>
-    //         <legend>${name}</legend>
-    //         <div>${text}</div>
-    //     </fieldset>    
-    // `);
+//     // document.writeln(`
+//     //     <fieldset>
+//     //         <legend>${name}</legend>
+//     //         <div>${text}</div>
+//     //     </fieldset>    
+//     // `);
 
-    let form = document.querySelector("form");
-    form.insertAdjacentHTML("afterend", `<fieldset>
-             <legend><b>${name}</b></legend>
-             <div>${text}</div>
-         </fieldset>  `);
+//     let form = document.querySelector("form");
+//     form.insertAdjacentHTML("afterend", `<fieldset>
+//              <legend><b>${name}</b></legend>
+//              <div>${text}</div>
+//          </fieldset>  `);
     
-}
+// }
 
 // let st = "  текст строки первой    ";
 // st = st.replace(/^\s+|\s+$/g, "");
@@ -4985,4 +4985,6 @@ function smsUser(){
 // let date = "25-10-2026, 14/09/2025, 10.03.2022, 12:02:2014";
 // let re = date.split(/[-\/,.:]/);
 // console.log(re);
+
+console.log("Вносим изменения");
 
